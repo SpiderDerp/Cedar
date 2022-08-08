2e70c28f23ca16f1ea39ee5f815659aa92648aef
SpiderDerp <sfsreeramrave@gmail.com>
2022-08-07 21:34:53 -0400

    Initial Commit

--- src/main.rs
+++ src/main.rs
@@ -16,5 +16,19 @@ fn main() -> Result<(), PlatformError> {
 }
 
 fn ui_builder() -> impl Widget<u32> {
+    let label = Label::new(|data: &u32, _env: &_| format!("Hello World {}", data))
+        .with_text_color(Color::rgb8(0, 0, 0))
+        .with_text_size(24.0);
+
+    let background = Painter::new(|ctx, _, env| {
+        let size = ctx.size();
+        let rect = size.to_rect();
+        ctx.fill(rect, &env.get(theme::BACKGROUND_LIGHT));
+    });
+
+    Flex::column()
+        .with_child(background)
+        .with_child(label)
+        .cross_axis_alignment(CrossAxisAlignment::Center)
 
 }
